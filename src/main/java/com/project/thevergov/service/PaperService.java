95f6d66f17f315d1a5b69adc26abbd4bdb77297a
miroslavvergov <gabberlt123@gmail.com>
2024-07-14 18:09:44 +0300

    PaperService interface

---
+++ src/main/java/com/project/thevergov/service/PaperService.java
@@ -0,0 +1,26 @@
+package com.project.thevergov.service;
+
+import com.project.thevergov.dto.api.iPaper;
+import org.springframework.core.io.Resource;
+import org.springframework.data.domain.Page;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface PaperService {
+
+    Page<iPaper> getPapers(int page, int size);
+
+    Page<iPaper> getPapers(int page, int size, String name);
+
+    Collection<iPaper> savePapers(String userId, List<MultipartFile> papers);
+
+    iPaper updatePaper(String paperId, String name, String description);
+
+    void deletePaper(String paperId);
+
+    iPaper getPaperByPaperId(String paperId);
+
+    Resource getResource(String documentName);
+}
