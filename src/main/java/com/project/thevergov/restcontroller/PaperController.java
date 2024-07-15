730a682c6440d92d5ddb611d091909df55c10882
miroslavvergov <gabberlt123@gmail.com>
2024-07-15 10:14:59 +0300

    Endpoint to save papers

---
+++ src/main/java/com/project/thevergov/restcontroller/PaperController.java
@@ -0,0 +1,55 @@
+package com.project.thevergov.restcontroller;
+
+import com.project.thevergov.domain.Response;
+import com.project.thevergov.dto.User;
+import com.project.thevergov.service.PaperService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+
+import static com.project.thevergov.utils.RequestUtils.getResponse;
+import static java.util.Collections.emptyMap;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/papers")
+public class PaperController {
+
+    private PaperService paperService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<Response> savePapers
+            (@AuthenticationPrincipal User user,
+             @RequestParam("files") List<MultipartFile> papers,
+             HttpServletRequest request
+            ) {
+
+        var savedPapers = paperService.savePapers(user.getUserId(), papers);
+
+        return ResponseEntity
+                .created(getUri())
+                .body(getResponse(request,
+                        Map.of("papers", savedPapers),
+                        "Paper(s) uploaded",
+                        CREATED));
+
+    }
+
+
+    private URI getUri() {
+        return URI.create("");
+    }
+
+}
