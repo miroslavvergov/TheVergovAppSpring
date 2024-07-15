ac8be31d58df60cf08aefec70f9f6a354a4886ee
miroslavvergov <gabberlt123@gmail.com>
2024-07-15 10:20:41 +0300

    Endpoint to fetch papers from db

--- src/main/java/com/project/thevergov/restcontroller/PaperController.java
+++ src/main/java/com/project/thevergov/restcontroller/PaperController.java
@@ -7,10 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.net.URI;
@@ -44,6 +41,25 @@ public class PaperController {
                         Map.of("papers", savedPapers),
                         "Paper(s) uploaded",
                         CREATED));
+
+    }
+
+    @GetMapping
+    public ResponseEntity<Response> getPapers
+            (@AuthenticationPrincipal User user,
+             HttpServletRequest request,
+             @RequestParam(value = "page", defaultValue = "0") int page,
+             @RequestParam(value = "size", defaultValue = "5") int size
+            ) {
+
+        var papers = paperService.getPapers(page, size);
+
+        return ResponseEntity
+                .ok()
+                .body(getResponse(request,
+                        Map.of("papers", papers),
+                        "Paper(s) fetched",
+                        OK));
 
     }
 
